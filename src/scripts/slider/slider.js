f7528e8c7ca85b17b3b4e072d78b15fab67fa1f3
DKukushkin91 <jame7on@gmail.com>
2022-03-11 18:56:48 +0500

    add slider

--- src/scripts/slider/slider.js
+++ src/scripts/slider/slider.js
@@ -1,3 +1,32 @@
 export const slider = () => {
-	return console.log('yepasdas');
+	if(document.querySelector('.js-slider')) {
+		const slides = document.querySelectorAll('.js-slide');
+		const prevButton = document.querySelector('.js-btn-prev');
+		const nextButton = document.querySelector('.js-btn-next');
+		const slidesLength = slides.length - 1;
+
+		let activeSlide = 0;
+
+		slides[activeSlide + 1].classList.add('section__slide--next');
+		slides[activeSlide].classList.add('section__slide--active');
+
+		const slideHandler = (evt) => {
+			if(evt.target === prevButton) {
+				activeSlide > 0 ? activeSlide-- : activeSlide = slidesLength;
+			} else {
+				activeSlide < slidesLength ? activeSlide++ : activeSlide = 0;
+			}
+
+			slides.forEach(el => {
+				el.classList.remove('section__slide--active');
+				el.classList.remove('section__slide--next');
+			})
+
+			slides[activeSlide].classList.add('section__slide--active');
+			slides[(activeSlide + 1) % slides.length].classList.add('section__slide--next');
+		}
+
+		nextButton.addEventListener('click', slideHandler);
+		prevButton.addEventListener('click', slideHandler);
+	}
 }
